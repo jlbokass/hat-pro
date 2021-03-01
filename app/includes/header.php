b5f3f33a1d43fb4856377cf2771877822d7a52f7
Bokassa Jean le Grand <j_dabok@me.com>
2021-03-01 16:05:15 +0100

    initial commit

---
+++ app/includes/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<!-- Primary Meta Tags -->
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge; text/html; charset=UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>HAT | Bienvenu</title>
+	<meta name="description" content="HAT, le réseau social d'entreprise centré sur l'humain" />
+	<meta name="keywords" content="HAT, Patrimoine immateriel, Ressources humaines" />
+	<meta name="author" content="Gil Petuaud Letang" />
+	
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://portfolio.jlgb.fr">
+	<meta property="og:title" content="HAT">
+	<meta property="og:description" content="HAT, le réseau social d'entreprise centré sur l'humain">
+	<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="https://portfolio.jlgb.fr">
+	<meta property="twitter:title" content="HAT">
+	<meta property="twitter:description" content="HAT, le réseau social d'entreprise centré sur l'humain">
+	<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+    <!-- My CSS -->
+    <link rel="stylesheet" href="../app/assets/styles/styles.css">
+
+	<!-- Google Fonts for title -->
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,600;1,200;1,300;1,400;1,600&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<div class="container">
+<header class="group">
+    <div class="logo">
+		<img src="../app/assets/images/logo-hat.png" alt="Logo HAT">
+	</div>
+	<div class="navigation">
+		<nav class="site-nav">
+			<ul class="group">
+				<li><a class="active" href="#">Accueil</a></li>
+				<li><a href="#">À propos</a></li>
+				<li><a href="#">Offre et tarifs</a></li>
+				<li><a href="#">Blog</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+		</nav>
+	</div>
+	<div class="nav-button">
+		<button class="btn">Stimuler vos besoins</button>
+	</div>
+</header>
---
+++ app/index.php
@@ -0,0 +1,27 @@
+<?php
+include_once 'includes/header.php';
+?>
+
+<div class="section">
+	<div class="description">
+		<h1>découvrez hat pro</h1>
+		<p class="lead">le réseau social d'entreprise centré sur l'humain, ses expériences et passions.</p>
+		<p>
+		Un outil numérique favorisant les liens dans la vie réelle de reconnaissance, d'appartenance et sens.
+		</p>
+		<p>
+			Des caractéristique spécifiques:
+		</p>
+		<ul>
+			<li>outil transversal</li>
+			<li>marque blanche</li>
+			<li>fonctionnalités sur mesures</li>
+		</ul>
+	</div>
+	<div class="img-description">
+
+	</div>
+</div>
+
+<?php
+include_once 'includes/footer.php';
